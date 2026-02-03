1690223ec67a44e7ed5037588beafabd9d7f8353
ZayyadAbdullahiBello <zayyadabdullahi2@gmail.com>
2026-02-03 12:23:59 +0100

    Added angular routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,15 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -20,34 +16,9 @@ import { User } from '../types/user';
 // }
 // ****FOLLOW COME CODE OO BUT CHANGED IT TO HOW IT IS IN THE TUTORIAL***************
 
-export class App implements OnInit{
+export class App{
   
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected title = 'Dating App';
-  protected members = signal<User[]>([]);
+  protected router = inject(Router);
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers())
-    this.setCurrentUser()
-  }
-
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers(){
-    try{
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members')) 
-    } catch(error){
-      console.log(error)
-      throw error
-    }
-  }
-
-  
 
 }

079228a17e87f53cbdfc0f0879ee8437781b0564
ZayyadAbdullahiBello <zayyadabdullahi2@gmail.com>
2026-02-07 10:30:36 +0100

    End of Section 8

---
+++ API/Interfaces/IMemberRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using API.Entities;
+// using API.Helpers;
+
+namespace API.Interfaces;
+
+public interface IMemberRepository
+{
+    void Update(Member member);
+    Task<bool> SaveAllAsync();
+    Task<IReadOnlyList<Member>> GetMembersAsync();
+    Task<Member?> GetMemberByIdAsync(string id);
+    Task<IReadOnlyList<Photo>> GetPhotosForMemberAsync(string memberId);
+    Task<Member?> GetMemberForUpdate(string id);
+}
